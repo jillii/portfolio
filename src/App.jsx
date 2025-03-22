b69ee257a84a7ddb9de2107b183739f04b14a2be
Jill <jho3292@gmail.com>
2025-03-22 15:53:33 -0400

    Add top nav.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
+import './assets/Nav.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
 
   return (
     <>
-    hi
+      <BrowserRouter>
+        <nav>
+          <div className='site-container'>
+            <ul>
+              <li><Link to="/"></Link></li>
+              <li><Link to="/about/">About</Link></li>
+              <li><Link to="/contact/">Contact</Link></li>
+            </ul>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about/" element={<About />}></Route>
+          <Route path="/contact/" element={<Contact />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Single.jsx
@@ -0,0 +1,8 @@
+function Single() {
+  return (
+    <div className="site-container">
+      <h1>Single Page</h1>
+    </div>
+  )
+}
+export default Single;
