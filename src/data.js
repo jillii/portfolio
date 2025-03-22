de8d2a6bcb0812ce09c4660e19f489617e910b2c
Jill <jho3292@gmail.com>
2025-03-22 12:16:58 -0400

    Initial commit.

---
+++ src/data.js
@@ -0,0 +1,85 @@
+export const ABOUT = {
+    name: "Jill Hoenig",
+    title: "Web Engineer",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum sapien dui."
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum sapien dui, eu facilisis justo convallis quis. Praesent et dignissim dui."
+}
+export const CONTACT = {
+    email: 'jho3292@gmail.com',
+    socials: {
+        GitHub: 'https://github.com/jillii',
+        Medium: 'https://medium.com/@jillii',
+        Stackoverflow: 'https://stackoverflow.com/users/5161457/jillian-hoenig',
+    }
+}
+export const WORK = [
+    {
+        title: "Beamble",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tech: ['PHP', 'SCSS', 'JavaScript', 'Git', 'CSS Framework Animations'],
+        link: 'https://beamable.com/'
+    },
+    {
+        title: "Cybernetix Playbook",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tech: ['PHP', 'SCSS', 'JavaScript', 'Git', 'CSS Framework Animations'],
+        link: 'https://theroboticsplaybook.com/'
+    },
+    {
+        title: "Quorum Federal Credit Union",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tech: ['PHP', 'SCSS', 'JavaScript', 'Git', 'CSS Framework Animations'],
+        link: 'https://www.quorumfcu.org/'
+    },
+    {
+        title: "Ideometry",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tech: ['PHP', 'SCSS', 'JavaScript', 'Git', 'CSS Framework Animations'],
+        link: 'https://ideometry.com/'
+    },
+    {
+        title: "Foxy AI",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tech: ['JavaScript'],
+        link: 'https://foxyai.com/'
+    },
+    {
+        title: "Allstacks",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tech: ['JavaScript'],
+        link: 'https://www.allstacks.com/roi-calculator'
+    },
+
+]
+export const PROJECTS = [
+    {
+        title: 'Playlists With Friends (WIP)',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum sapien dui, eu facilisis justo convallis quis. Praesent et dignissim dui.',
+        tech: ['JavaScript', 'Ruby', 'Rails', 'YouTube API', 'Git', 'Dokku'],
+        link: 'https://playlistswithfriends.app/'
+    },
+    {
+        title: 'Guess The Phrase',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum sapien dui, eu facilisis justo convallis quis. Praesent et dignissim dui.',
+        tech: ['JavaScript', 'React', 'Git'],
+        link: 'https://jillii.github.io/guess-the-phrase/'
+    },
+    {
+        title: '!@#$% Official',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum sapien dui, eu facilisis justo convallis quis. Praesent et dignissim dui.',
+        tech: ['JavaScript', 'CSS', 'Jekyll', 'Liquid', 'CSS Framework Animation', 'Git'],
+        link: 'https://expletive.plumbing/'
+    },
+    {
+        title: 'Cake Factory Official',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum sapien dui, eu facilisis justo convallis quis. Praesent et dignissim dui.',
+        tech: ['JavaScript', 'CSS', 'Jekyll', 'Liquid', 'Firebase', 'Git'],
+        link: 'https://cakefactory.party/'
+    },
+    {
+        title: 'Design Statements LLC Official',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum sapien dui, eu facilisis justo convallis quis. Praesent et dignissim dui.',
+        tech: ['WordPress', 'PHP', 'Git'],
+        link: 'http://designstatements-llc.com/'
+    },
+]
