30be4dbe1b4a0920f5bc7d803e0c8cb29dc61408
Jill <jho3292@gmail.com>
2025-03-28 11:00:41 -0400

    Fix spelling.

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,3 +1,3 @@
 export default function Status () {
-    return (<div className="status"><p className="status-text" data-desktop="Available for new oportunities" data-mobile="Available"></p></div>)
+    return (<div className="status"><p className="status-text" data-desktop="Available for new opportunities" data-mobile="Available"></p></div>)
 }
