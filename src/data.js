b06c12b54581718291688ae70c87611e18230b30
Jill <jho3292@gmail.com>
2025-04-30 12:35:22 -0400

    Update summary verbiage.

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ export const ABOUT = {
     bio: 
     [
         "I'm Jill—Web engineer. 10 years agency experience.",
-        "I'm a Boston-based fullstack engineer, with a giant passion and focus on frontend.",
+        "Boston-based fullstack engineer, with a giant passion and focus on frontend.",
         "I've built award-winning websites for major agencies and helped SMEs grow with polished, high-performance sites.",
 
         "I lived through the dawn of the internet, and it captures my imagination to this day.",
