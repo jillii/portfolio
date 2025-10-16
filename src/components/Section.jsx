d7e640ef8d7df160f3a0ad5305b6c06f451cb7fe
Jill <jho3292@gmail.com>
2025-10-16 14:01:34 -0400

    Remove padding and blur when section head elements aren't covering cards.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import Card from './Card'
 import Filter from './Filter'
 
 export default function Section (props) {
     const title = props.title
+    const headerRef = useRef(null)
     let filterTech = []
     const initial = props.list
     const [list, setList] = useState(initial)
+    const [isSticky, setIsSticky] = useState('')
+    
+    if (headerRef.current) {
+        const observer = new window.IntersectionObserver(([entry]) => {
+            const top = entry.boundingClientRect.top
+            if (top === 102) {
+                setIsSticky(' is-sticky')
+            } else {
+                setIsSticky('')
+            }
+        })
+        observer.observe(headerRef.current)
+    }
 
     return (
         <section className="card-section">
-            <div className='site-container section-head glass-container'>
+            <div className={`site-container section-head glass-container${isSticky}`} ref={headerRef}>
                 <div className=" glass">
                     <h2>{title}</h2>
                 </div>
