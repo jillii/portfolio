6320c9e757c5439ea5c26c8686aca69f9883ccc3
Jill <jho3292@gmail.com>
2026-06-14 13:52:24 -0400

    Move filter tech filter above cards.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,26 +5,30 @@ import Filter from './Filter'
 export default function Section (props) {
     const title = props.title
     const headerRef = useRef(null)
-    let filterTech = []
     const initial = props.list
     const [list, setList] = useState(initial)
+    let filterTech = []
+    list.map(item => {
+        item.tech.map(tech => {
+            if (filterTech.indexOf(tech) === -1) filterTech.push(tech) // Add tech is not already added
+        })
+    })
 
     return (
         <section className="card-section">
             <div className='site-container section-head'>
                 <h2>{title}</h2>
             </div>
-            <div className="card-container site-container">
-                <div class="card-track">
-                    {list.map((item, index) => {
-                        {item.tech.map((tech) => filterTech.indexOf(tech) === -1 ? filterTech.push(tech) : '')}
-                        return (<Card key={index} slug={item.slug} title={item.title} tech={item.tech} body={item.body} image={item.image} overlay={item.overlay} />)
-                    })}
-                </div>
-            </div>
             <div className='site-container'>
                 <label>Filter by language / technology:</label>
                 <Filter list={list} setList={setList} filterTech={filterTech} initial={initial} />
+            </div>
+            <div className="card-container site-container">
+                <div class="card-track">
+                    {list.map((item, index) => {
+                        return (<Card key={index} slug={item.slug} title={item.title} tech={item.tech} body={item.body} image={item.image} overlay={item.overlay} />)
+                    })}
+                </div>
             </div>
         </section>
     )
